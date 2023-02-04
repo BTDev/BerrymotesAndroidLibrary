39e6fc90ed764cd16176dec09d8094cdb4843f44
Atte Virtanen <aten.email@gmail.com>
2023-02-04 12:53:51 +0200

    Update SDK and build tools

--- src/main/java/com/trellmor/berrymotes/AnimationTextView.java
+++ src/main/java/com/trellmor/berrymotes/AnimationTextView.java
@@ -25,7 +25,7 @@ import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.Editable;
 import android.text.Spanned;
 import android.text.TextWatcher;
